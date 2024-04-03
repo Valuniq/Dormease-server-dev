1e93b79da7e37b93b675671bc8e1dc31d76900c2
강승우 <phonil@naver.com>
2024-04-03 19:52:27 +0900

    [FEAT]: 프로젝트 세팅

---
+++ src/main/java/dormease/dormeasedev/DormeaseDevApplication.java
@@ -0,0 +1,13 @@
+package dormease.dormeasedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormeaseDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormeaseDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dormease/dormeasedev/DormeaseDevApplicationTests.java
@@ -0,0 +1,13 @@
+package dormease.dormeasedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormeaseDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
