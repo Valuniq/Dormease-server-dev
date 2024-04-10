03709ed22cc2b56e477a5364dd59939989b15fa0
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-04-10 12:48:54 +0900

    [CHORE] ErrorCode 추가

--- src/main/java/dormease/dormeasedev/global/payload/ErrorCode.java
+++ src/main/java/dormease/dormeasedev/global/payload/ErrorCode.java
@@ -10,7 +10,10 @@ public enum ErrorCode {
     INVALID_FILE_PATH(400, null, "잘못된 파일 경로 입니다."),
     INVALID_OPTIONAL_ISPRESENT(400, null, "해당 값이 존재하지 않습니다."),
     INVALID_CHECK(400, null, "해당 값이 유효하지 않습니다."),
-    INVALID_AUTHENTICATION(400, null, "잘못된 인증입니다.");
+    INVALID_AUTHENTICATION(400, null, "잘못된 인증입니다."),
+
+    // NOT_FOUND(404, null, "해당 객체를 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(500, null, "서버에서 예상치 못한 오류가 발생했습니다.");
 
     private final String code;
     private final String message;
