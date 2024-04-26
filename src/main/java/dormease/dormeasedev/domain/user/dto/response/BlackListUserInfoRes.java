7e7994c9a4b4f21452446d9d42d3d1c8e0f7a57f
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-04-26 18:46:36 +0900

    [CHORE] 스웨거 명세 추가

--- src/main/java/dormease/dormeasedev/domain/user/dto/response/BlackListUserInfoRes.java
+++ src/main/java/dormease/dormeasedev/domain/user/dto/response/BlackListUserInfoRes.java
@@ -1,5 +1,6 @@
 package dormease.dormeasedev.domain.user.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,22 +11,26 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class BlackListUserInfoRes {
 
+    @Schema(type = "Long", example = "1", description= "회원의 id입니다.")
     private Long id;
 
+    @Schema(type = "String", example = "사용자", description= "회원의 이름입니다.")
     private String name;
 
+    @Schema(type = "String", example = "60xxxxxx", description= "회원의 학번(또는 수험번호)입니다.")
     private String studentNumber;
 
-    // 휴대전화
+    @Schema(type = "String", example = "전화번호", description= "회원의 전화번호입니다.")
     private String phoneNumber;
 
-    // 벌점
+    @Schema(type = "Integer", example = "1", description= "회원의 벌점 총점입니다.")
     private Integer minusPoint;
 
-    // 사유
+    @Schema(type = "String", example = "관내 흡연", description= "회원의 블랙리스트 등록 사유입니다.")
     private String content;
 
     // 생성 일자
+    @Schema(type = "LocalDate", example = "2024-04-26", description = "블랙리스트 등록 일자입니다.")
     private LocalDate createdAt;
 
     @Builder
