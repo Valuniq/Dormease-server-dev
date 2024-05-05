f23cb1b000583f5d4876d56daa55f42573393e42
강승우 <phonil@naver.com>
2024-05-06 03:54:44 +0900

    [FEAT]: APP - 룸메이트 그룹 생성 (룸메이트 임시 신청 테이블 + 코드 발급 포함)

---
+++ src/main/java/dormease/dormeasedev/domain/roommate_temp_application/controller/RoommateTempApplicationController.java
@@ -0,0 +1,50 @@
+package dormease.dormeasedev.domain.roommate_temp_application.controller;
+
+import dormease.dormeasedev.domain.roommate_temp_application.service.RoommateTempApplicationService;
+import dormease.dormeasedev.global.config.security.token.CustomUserDetails;
+import dormease.dormeasedev.global.payload.ErrorResponse;
+import dormease.dormeasedev.global.payload.Message;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Roommate Temp Application API", description = "APP - 룸메이트 임시 신청 관련 API입니다.")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/app/roommateTempApplication")
+public class RoommateTempApplicationController {
+
+    private final RoommateTempApplicationService roommateTempApplicationService;
+
+    // Description : 그룹 생성 (코드 발급) (방장 o)
+    @Operation(summary = "그룹 생성 (코드 발급)", description = "룸메이트 그룹을 생성합니다. (코드 발급)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "그룹 생성 (코드 발급) 성공", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))}),
+            @ApiResponse(responseCode = "400", description = "그룹 생성 (코드 발급) 실패", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+    })
+    @PostMapping
+    public ResponseEntity<?> createRoommateTempApplication(
+            @Parameter(description = "Access Token을 입력해주세요.", required = true) @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        return roommateTempApplicationService.createRoommateTempApplication(customUserDetails);
+    }
+
+    // Description : 룸메이트 신청 (방장 o)
+
+    // Description : 그룹 삭제 (방장 o)
+
+    // Description : 코드 입력 (방장 x)
+
+    // Description : 그룹 나가기 (방장 x)
+
+}
---
+++ src/main/java/dormease/dormeasedev/domain/roommate_temp_application/service/RoommateTempApplicationService.java
@@ -0,0 +1,67 @@
+package dormease.dormeasedev.domain.roommate_temp_application.service;
+
+import dormease.dormeasedev.domain.roommate_temp_application.domain.RoommateTempApplication;
+import dormease.dormeasedev.domain.roommate_temp_application.domain.repository.RoommateTempApplicationRepository;
+import dormease.dormeasedev.domain.user.domain.User;
+import dormease.dormeasedev.domain.user.service.UserService;
+import dormease.dormeasedev.global.config.security.token.CustomUserDetails;
+import dormease.dormeasedev.global.payload.ApiResponse;
+import dormease.dormeasedev.global.payload.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.SecureRandom;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class RoommateTempApplicationService {
+
+    private final RoommateTempApplicationRepository roommateTempApplicationRepository;
+
+    private final UserService userService;
+
+    @Transactional
+    public ResponseEntity<?> createRoommateTempApplication(CustomUserDetails customUserDetails) {
+
+        User user = userService.validateUserById(customUserDetails.getId());
+
+        String code;
+        Optional<RoommateTempApplication> findRoommateTempApplication;
+        do {
+            code = generateCode();
+            findRoommateTempApplication = roommateTempApplicationRepository.findByCode(code);
+        } while (findRoommateTempApplication.isPresent());
+
+        RoommateTempApplication roommateTempApplication = RoommateTempApplication.builder()
+                .code(code)
+                .isApplied(false)
+                .roommateMasterId(user.getId())
+                .build();
+
+        roommateTempApplicationRepository.save(roommateTempApplication);
+
+        ApiResponse apiResponse = ApiResponse.builder()
+                .check(true)
+                .information(Message.builder().message("그룹 생성이 완료되었습니다.").build())
+                .build();
+
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    public String generateCode() {
+
+        SecureRandom random = new SecureRandom();
+        // nextInt(int bound) : 0(포함)부터 입력된 bound(미포함) 사이의 랜덤 정수를 반환
+        int randomNumber = random.nextInt(100000000);
+
+        // 숫자를 8자리 문자열로 포맷팅
+        // %08d는 8자리의 정수 / 부족한 자리는 0으로 채워짐
+        String code = String.format("%08d", randomNumber);
+
+        return code;
+    }
+}
