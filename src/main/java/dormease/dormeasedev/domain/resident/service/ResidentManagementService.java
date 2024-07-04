13def96c922f2e14f8b5ece02202a695bc051a17
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-07-05 00:34:55 +0900

    [FEAT] 사생 조회/검색 및 정렬 기능 구현

--- src/main/java/dormease/dormeasedev/domain/resident/service/ResidentManagementService.java
+++ src/main/java/dormease/dormeasedev/domain/resident/service/ResidentManagementService.java
@@ -1,5 +1,9 @@
 package dormease.dormeasedev.domain.resident.service;
 
+import dormease.dormeasedev.domain.dormitory_application.domain.DormitoryApplication;
+import dormease.dormeasedev.domain.dormitory_application.domain.DormitoryApplicationResult;
+import dormease.dormeasedev.domain.dormitory_application.domain.repository.DormitoryApplicationRepository;
+import dormease.dormeasedev.domain.dormitory_application_setting.domain.ApplicationStatus;
 import dormease.dormeasedev.domain.point.dto.response.ResidentInfoRes;
 import dormease.dormeasedev.domain.resident.domain.Resident;
 import dormease.dormeasedev.domain.resident.domain.repository.ResidentRepository;
@@ -27,6 +31,7 @@ import java.util.stream.Collectors;
 public class ResidentManagementService {
 
     private final ResidentRepository residentRepository;
+    private final DormitoryApplicationRepository dormitoryApplicationRepository;
     private final UserService userService;
 
     // 사생 직접 추가
@@ -34,28 +39,64 @@ public class ResidentManagementService {
     // 사생의 성별에 맞는 건물 조회
     // 사생 상세 조회
 
-    // 사생 목록 조회
+    // 사생 목록 조회 및 정렬
+    // Description : 기본(sortBy: Name / isAscending: true)
     // TODO : 설정한 퇴사 날짜 자정이 되기 10분 전에 해당 기간의 사생 데이터는 삭제
-    public ResponseEntity<?> getResidents(CustomUserDetails customUserDetails, Integer page) {
+    public ResponseEntity<?> getResidents(CustomUserDetails customUserDetails, String sortBy, Boolean isAscending, Integer page) {
         User admin = userService.validateUserById(customUserDetails.getId());
         Pageable pageable = PageRequest.of(page, 25);
         // 사생 목록 조회 (페이징 적용)
         Page<Resident> residents = residentRepository.findByUserSchool(admin.getSchool(), pageable);
 
         List<ResidentRes> residentResList = residents.getContent().stream()
-                .map(resident -> ResidentRes.builder()
-                        .residentId(resident.getId())
-                        .name(resident.getUser().getName())
-                        .studentNumber(resident.getUser().getStudentNumber())
-                        .gender(resident.getUser().getGender())
-                        .bonusPoint(resident.getUser().getBonusPoint())
-                        .minusPoint(resident.getUser().getMinusPoint())
-                        .dormitory(resident.getRoom().getDormitory().getName() + "(" + resident.getRoom().getDormitory().getRoomSize() + "인실)")
-                        .room(resident.getRoom().getRoomNumber())
-                        .schoolStatus(resident.getUser().getSchoolStatus())
-                        .build())
-                .sorted(Comparator.comparing(ResidentRes::getName))
+                .map(resident -> {
+                    String dormitory;
+                    Integer room;
+                    // 호실 미배정
+                    if (resident.getRoom() == null) {
+                        DormitoryApplication dormitoryApplication = dormitoryApplicationRepository.findByUserAndApplicationStatusAndDormitoryApplicationResult(resident.getUser(), ApplicationStatus.NOW, DormitoryApplicationResult.PASS);
+                        // TODO: 호실 미배정 시 인실 표기는 어떻게?
+                        dormitory = dormitoryApplication.getDormitory().getName() +
+                                    "(" + dormitoryApplication.getDormitory().getRoomSize() + "인실)";
+                        room = null;
+                    } else {
+                        dormitory = resident.getRoom().getDormitory().getName() +
+                                "(" + resident.getRoom().getDormitory().getRoomSize() + "인실)";
+                        room = resident.getRoom().getRoomNumber();
+                    }
+
+                    return ResidentRes.builder()
+                            .residentId(resident.getId())
+                            .name(resident.getUser().getName())
+                            .studentNumber(resident.getUser().getStudentNumber())
+                            .gender(resident.getUser().getGender())
+                            .bonusPoint(resident.getUser().getBonusPoint())
+                            .minusPoint(resident.getUser().getMinusPoint())
+                            .dormitory(dormitory)
+                            .room(room)
+                            .schoolStatus(resident.getUser().getSchoolStatus())
+                            .build();
+                })
                 .collect(Collectors.toList());
+
+        // 정렬
+        Comparator<ResidentRes> comparator = switch (sortBy) {
+            case "bonusPoint" ->
+                    Comparator.comparing(ResidentRes::getBonusPoint, Comparator.nullsLast(Comparator.naturalOrder()));
+            case "minusPoint" ->
+                    Comparator.comparing(ResidentRes::getMinusPoint, Comparator.nullsLast(Comparator.naturalOrder()));
+            case "dormitory" ->
+                    Comparator.comparing(ResidentRes::getDormitory, Comparator.nullsLast(Comparator.naturalOrder()));
+            case "gender" ->
+                    Comparator.comparing(ResidentRes::getGender, Comparator.nullsLast(Comparator.naturalOrder()));
+            default -> Comparator.comparing(ResidentRes::getName, Comparator.nullsLast(Comparator.naturalOrder()));
+        };
+
+        if (!isAscending) {
+            comparator = comparator.reversed();
+        }
+
+        residentResList.sort(comparator);
 
         ApiResponse apiResponse = ApiResponse.builder()
                 .check(true)
@@ -64,35 +105,9 @@ public class ResidentManagementService {
         return ResponseEntity.ok(apiResponse);
     }
 
-    public ResponseEntity<?> getSortedResidents(CustomUserDetails customUserDetails, String sortBy, Boolean isAscending, Integer page) {
-        User admin = userService.validateUserById(customUserDetails.getId());
-        String sortField = "user." + sortBy;
-        Pageable pageable = PageRequest.of(page, 25, isAscending ? Sort.Direction.ASC : Sort.Direction.DESC, sortField);
-        // 사생 목록 조회 (페이징 적용)
-        Page<Resident> residents = residentRepository.findByUserSchool(admin.getSchool(), pageable);
-
-        List<ResidentRes> residentResList = residents.getContent().stream()
-                .map(resident -> ResidentRes.builder()
-                        .residentId(resident.getId())
-                        .name(resident.getUser().getName())
-                        .studentNumber(resident.getUser().getStudentNumber())
-                        .gender(resident.getUser().getGender())
-                        .bonusPoint(resident.getUser().getBonusPoint())
-                        .minusPoint(resident.getUser().getMinusPoint())
-                        .dormitory(resident.getRoom().getDormitory().getName() + "(" + resident.getRoom().getDormitory().getRoomSize() + "인실)")
-                        .room(resident.getRoom().getRoomNumber())
-                        .schoolStatus(resident.getUser().getSchoolStatus())
-                        .build())
-                .collect(Collectors.toList());
-
-        ApiResponse apiResponse = ApiResponse.builder()
-                .check(true)
-                .information(residentResList).build();
-
-        return ResponseEntity.ok(apiResponse);
-    }
-
-    public ResponseEntity<?> getSearchResidents(CustomUserDetails customUserDetails, String keyword, Integer page) {
+    // 검색 및 정렬
+    // Description : 기본(sortBy: Name / isAscending: true)
+    public ResponseEntity<?> getSearchResidents(CustomUserDetails customUserDetails, String keyword, String sortBy, Boolean isAscending, Integer page) {
         User admin = userService.validateUserById(customUserDetails.getId());
         String cleanedKeyword = keyword.trim().toLowerCase();;
 
@@ -101,19 +116,53 @@ public class ResidentManagementService {
         Page<Resident> residents = residentRepository.searchResidentsByKeyword(admin.getSchool(), cleanedKeyword, pageable);
 
         List<ResidentRes> residentResList = residents.getContent().stream()
-                .map(resident -> ResidentRes.builder()
-                        .residentId(resident.getId())
-                        .name(resident.getUser().getName())
-                        .studentNumber(resident.getUser().getStudentNumber())
-                        .gender(resident.getUser().getGender())
-                        .bonusPoint(resident.getUser().getBonusPoint())
-                        .minusPoint(resident.getUser().getMinusPoint())
-                        .dormitory(resident.getRoom().getDormitory().getName() + "(" + resident.getRoom().getDormitory().getRoomSize() + "인실)")
-                        .room(resident.getRoom().getRoomNumber())
-                        .schoolStatus(resident.getUser().getSchoolStatus())
-                        .build())
-                .sorted(Comparator.comparing(ResidentRes::getName))
+                .map(resident -> {
+                    String dormitory;
+                    Integer room;
+                    // 호실 미배정
+                    if (resident.getRoom() == null) {
+                        DormitoryApplication dormitoryApplication = dormitoryApplicationRepository.findByUserAndApplicationStatusAndDormitoryApplicationResult(resident.getUser(), ApplicationStatus.NOW, DormitoryApplicationResult.PASS);
+                        dormitory = dormitoryApplication.getDormitory().getName() +
+                                "(" + dormitoryApplication.getDormitory().getRoomSize() + "인실)";
+                        room = null;
+                    } else {
+                        dormitory = resident.getRoom().getDormitory().getName() +
+                                "(" + resident.getRoom().getDormitory().getRoomSize() + "인실)";
+                        room = resident.getRoom().getRoomNumber();
+                    }
+
+                    return ResidentRes.builder()
+                            .residentId(resident.getId())
+                            .name(resident.getUser().getName())
+                            .studentNumber(resident.getUser().getStudentNumber())
+                            .gender(resident.getUser().getGender())
+                            .bonusPoint(resident.getUser().getBonusPoint())
+                            .minusPoint(resident.getUser().getMinusPoint())
+                            .dormitory(dormitory)
+                            .room(room)
+                            .schoolStatus(resident.getUser().getSchoolStatus())
+                            .build();
+                })
                 .collect(Collectors.toList());
+
+        // 정렬
+        Comparator<ResidentRes> comparator = switch (sortBy) {
+            case "bonusPoint" ->
+                    Comparator.comparing(ResidentRes::getBonusPoint, Comparator.nullsLast(Comparator.naturalOrder()));
+            case "minusPoint" ->
+                    Comparator.comparing(ResidentRes::getMinusPoint, Comparator.nullsLast(Comparator.naturalOrder()));
+            case "dormitory" ->
+                    Comparator.comparing(ResidentRes::getDormitory, Comparator.nullsLast(Comparator.naturalOrder()));
+            case "gender" ->
+                    Comparator.comparing(ResidentRes::getGender, Comparator.nullsLast(Comparator.naturalOrder()));
+            default -> Comparator.comparing(ResidentRes::getName, Comparator.nullsLast(Comparator.naturalOrder()));
+        };
+
+        if (!isAscending) {
+            comparator = comparator.reversed();
+        }
+
+        residentResList.sort(comparator);
 
         ApiResponse apiResponse = ApiResponse.builder()
                 .check(true)
