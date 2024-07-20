77b6372c1cf2aa1f0c27a6b82b55fcdfd74b4229
강승우 <phonil@naver.com>
2024-07-20 22:28:42 +0900

    [CHORE]: 주석 제거

--- src/main/java/dormease/dormeasedev/domain/notification/service/NotificationAppService.java
+++ src/main/java/dormease/dormeasedev/domain/notification/service/NotificationAppService.java
@@ -21,7 +21,6 @@ import dormease.dormeasedev.global.payload.ApiResponse;
 import dormease.dormeasedev.global.payload.PageInfo;
 import dormease.dormeasedev.global.payload.PageResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cglib.core.Block;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
