9c948820d8b177148202198fd9b0dabc96e4bffb
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-09-15 17:43:49 +0900

    [REFACTOR] `resident` 테이블 변경으로 인한 미배정 사생 조회 로직 수정

--- src/main/java/dormease/dormeasedev/domain/dormitory_applications/dormitory_term/domain/repository/DormitoryTermRepository.java
+++ src/main/java/dormease/dormeasedev/domain/dormitory_applications/dormitory_term/domain/repository/DormitoryTermRepository.java
@@ -1,6 +1,6 @@
 package dormease.dormeasedev.domain.dormitory_applications.dormitory_term.domain.repository;
 
-import dormease.dormeasedev.domain.dormitories.dormitory.domain.Dormitory;
+import dormease.dormeasedev.domain.dormitories.dormitory_room_type.domain.DormitoryRoomType;
 import dormease.dormeasedev.domain.dormitory_applications.dormitory_term.domain.DormitoryTerm;
 import dormease.dormeasedev.domain.dormitory_applications.term.domain.Term;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,5 +14,6 @@ public interface DormitoryTermRepository extends JpaRepository<DormitoryTerm, Lo
 
     List<DormitoryTerm> findByTerm(Term term);
 
-//    Optional<DormitoryTerm> findByTermAndDormitory(Term term, Dormitory dormitory);
+    List<DormitoryTerm> findByDormitoryRoomType(DormitoryRoomType dormitoryRoomType);
+
 }
