cc27f4d4c9511c4fd07b02402ed36fad3750cb75
강승우 <phonil@naver.com>
2024-09-25 13:04:41 +0900

    [FIX]: 합격자 명단 페이지 진입 시 입사신청설정 id 응답

--- src/main/java/dormease/dormeasedev/domain/dormitory_applications/dormitory_application/controller/PassDormitoryApplicationWebApi.java
+++ src/main/java/dormease/dormeasedev/domain/dormitory_applications/dormitory_application/controller/PassDormitoryApplicationWebApi.java
@@ -1,7 +1,9 @@
 package dormease.dormeasedev.domain.dormitory_applications.dormitory_application.controller;
 
 import dormease.dormeasedev.domain.dormitory_applications.dormitory_application.dto.response.DormitoryApplicationDormitoryRes;
+import dormease.dormeasedev.domain.dormitory_applications.dormitory_application.dto.response.PassAllDormitoryApplicationRes;
 import dormease.dormeasedev.domain.dormitory_applications.dormitory_application.dto.response.PassDormitoryApplicationRes;
+import dormease.dormeasedev.domain.exit_requestments.exit_requestment.dto.response.ExitRequestmentResidentRes;
 import dormease.dormeasedev.global.exception.ExceptionResponse;
 import dormease.dormeasedev.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,9 +25,12 @@ public interface PassDormitoryApplicationWebApi {
 
     @Operation(summary = "전체 합격자 목록 조회 API", description = "전체 합격자 목록을 조회합니다.")
     @ApiResponses(value = {
+            @ApiResponse(responseCode = "0", description = "전체 합격자 목록 조회 성공 - PassDormitoryApplicationRes 구성",
+                    content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PassDormitoryApplicationRes.class)))}
+            ),
             @ApiResponse(
                     responseCode = "200", description = "전체 합격자 목록 조회 성공",
-                    content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PassDormitoryApplicationRes.class)))}
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = PassAllDormitoryApplicationRes.class))}
             ),
             @ApiResponse(
                     responseCode = "400", description = "전체 합격자 목록 조회 실패",
---
+++ src/main/java/dormease/dormeasedev/domain/dormitory_applications/dormitory_application/dto/response/PassAllDormitoryApplicationRes.java
@@ -0,0 +1,20 @@
+package dormease.dormeasedev.domain.dormitory_applications.dormitory_application.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+public class PassAllDormitoryApplicationRes {
+
+    @Schema(type = "Long", example = "1", description = "입사 신청 설정 ID")
+    private Long dormitoryApplicationSettingId;
+
+    @Schema(type = "PassDormitoryApplicationRes", description = "입금 완료 / 합격자 목록입니다.")
+    List<PassDormitoryApplicationRes> passDormitoryApplicationResList;
+}
